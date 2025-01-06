8d28ddee54841a84df3cfadd2a9b85a601b57e48
jonfr <jonfjakobsen@gmail.com>
2025-01-06 21:07:30 +0100

    added option to set name

--- pdf_streamer.py
+++ pdf_streamer.py
@@ -13,8 +13,6 @@ cloned_speakers = {}
 from threading import Event
 
 stop_event = Event()
-header_slider = gr.Slider(label="Header Height", minimum=0, maximum=200, value=50, step=1)
-footer_slider = gr.Slider(label="Footer Height", minimum=0, maximum=200, value=60, step=1)
 
 print("Preparing file structure...")
 if not os.path.exists(OUTPUT):
@@ -110,7 +108,7 @@ def read_pdf_with_plumber(file_path, pages=None, header_height=50, footer_height
 
 
 
-def split_text_into_chunks(text, max_chars=250, max_tokens=300):
+def split_text_into_chunks(text, max_chars=250, max_tokens=350):
     """
     Split text into chunks of up to max_chars characters and max_tokens tokens,
     ensuring the split happens at sentence boundaries if possible.
@@ -170,24 +168,50 @@ def parse_page_range(page_range):
     return pages
 
 
+def combine_audio_files(output_dir, combined_filename="combined_audio.wav"):
+    """Combine all audio files in a directory into a single file."""
+    from pydub import AudioSegment
 
-def generate_audio_from_pdf(pdf_file, output_dir, speaker_type, speaker_name_studio, speaker_name_custom, lang, page_range, header_height, footer_height):
+    audio_files = [
+        os.path.join(output_dir, f) for f in os.listdir(output_dir) if f.endswith('.wav')
+    ]
+    if not audio_files:
+        return "No audio files found to combine."
+
+    combined = AudioSegment.empty()
+    for file in sorted(audio_files):
+        try:
+            audio = AudioSegment.from_file(file)
+            combined += audio
+        except Exception as e:
+            print(f"Error processing {file}: {e}")
+
+    combined_file_path = os.path.join(output_dir, combined_filename)
+    combined.export(combined_file_path, format="wav")
+    return f"Combined audio saved at {combined_file_path}"
+
+
+def generate_audio_from_pdf(pdf_file, output_dir, speaker_type, speaker_name_studio, speaker_name_custom, lang,
+                            page_range, header_height, footer_height, audio_file_name):
     """Generate audio for each text chunk in specified pages of a PDF."""
     stop_event.clear()
-    pdf_path = pdf_file.name  # Access the shared PDF file path
-    pdf_title = os.path.splitext(os.path.basename(pdf_path))[0]  # Get the PDF title without extension
-    pages = parse_page_range(page_range)  # Parse the page range
+
+    pdf_path = pdf_file.name
+    pdf_title = os.path.splitext(os.path.basename(pdf_path))[0]
+
+    # Use the provided name or default to the PDF name
+    base_audio_name = audio_file_name.strip() if audio_file_name.strip() else pdf_title
+
+    pages = parse_page_range(page_range)
+
     text = read_pdf_with_plumber(pdf_path, pages=pages, header_height=header_height, footer_height=footer_height)
     chunks = split_text_into_chunks(text)
-    embeddings = STUDIO_SPEAKERS[speaker_name_studio] if speaker_type == 'Studio' else cloned_speaker_names[speaker_name_custom]
+    embeddings = STUDIO_SPEAKERS[speaker_name_studio] if speaker_type == 'Studio' else cloned_speakers[
+        speaker_name_custom]
 
     for i, chunk in enumerate(chunks):
         if stop_event.is_set():
-            print("Process stopped by user.")
             break
-
-        if len(chunk) > 250:
-            print(f"Warning: Chunk {i + 1} exceeds 250 characters and might cause truncation.")
 
         response = requests.post(
             SERVER_URL + "/tts",
@@ -200,23 +224,22 @@ def generate_audio_from_pdf(pdf_file, output_dir, speaker_type, speaker_name_stu
         )
 
         if response.status_code != 200:
-            print(f"Error generating audio for chunk {i + 1}: {response.json()}")
             continue
 
         try:
             generated_audio = response.content
             decoded_audio = base64.b64decode(generated_audio)
         except base64.binascii.Error as e:
-            print(f"Error decoding base64 for chunk {i + 1}: {e}")
+            print(f"Error decoding audio chunk {i + 1}: {e}")
             continue
 
-        # Create a unique filename using the PDF title and chunk number
-        audio_filename = f"{pdf_title}_{i + 1}.wav"
+        audio_filename = f"{base_audio_name}_{i + 1}.wav"
         audio_path = os.path.join(output_dir, audio_filename)
         with open(audio_path, "wb") as audio_file:
             audio_file.write(decoded_audio)
-        print(f"Saved audio chunk {i + 1} to {audio_path}")
 
+    combined_filename = f"{base_audio_name}_combined.wav"
+    combine_audio_files(output_dir, combined_filename)
 
 
 def stop_process():
@@ -246,10 +269,6 @@ def preview_pdf_with_area(pdf_file, header_height, footer_height, page_number):
         img.save(temp_image_path)
         return temp_image_path
 
-
-
-
-
 with gr.Blocks() as demo:
     cloned_speaker_names = gr.State(list(cloned_speakers.keys()))
 
@@ -264,11 +283,13 @@ with gr.Blocks() as demo:
         label="Cloned speaker",
         choices=cloned_speaker_names.value,
         value=cloned_speaker_names.value[0] if len(cloned_speaker_names.value) != 0 else None,
+
     )
+    process_status = gr.Label(value="Idle", label="Process Status")
 
     # TTS Tab
     with gr.Tab("TTS"):
-        with gr.Column() as row4:
+        with gr.Column() as col1:
             speaker_name_studio
             speaker_name_custom
             speaker_type = gr.Dropdown(label="Speaker type", choices=["Studio", "Cloned"], value="Studio")
@@ -278,42 +299,57 @@ with gr.Blocks() as demo:
 
     # Clone Speaker Tab
     with gr.Tab("Clone a new speaker"):
-        with gr.Column() as col1:
+        with gr.Column() as col2:
             upload_file = gr.Audio(label="Upload reference audio", type="filepath")
             clone_speaker_name = gr.Textbox(label="Speaker name", value="default_speaker")
             clone_button = gr.Button(value="Clone speaker")
 
-
-
     # Generate Audio Tab
     with gr.Tab("Generate Audio from PDF"):
-        # Shared components
-        pdf_file = gr.File(label="Upload PDF", type="filepath")  # Shared PDF upload
-        header_slider = gr.Slider(label="Header Height", minimum=0, maximum=400, value=50, step=1)
-        footer_slider = gr.Slider(label="Footer Height", minimum=0, maximum=400, value=50, step=1)
-        page_range = gr.Textbox(label="Page Range (e.g., 1-3,5)", value="1", placeholder="Enter pages to process")
-        preview_image = gr.Image(label="Content Area Preview")
-        page_selector = gr.Number(label="Page Number", value=1, precision=0)  # Page selector for preview
 
-        # Optionally include a preview button (manual refresh)
-        preview_button = gr.Button(value="Refresh Preview")
+        with gr.Row() as row1:
+            with gr.Column() as col3:
+            # Shared components
+                pdf_file = gr.File(label="Upload PDF", type="filepath")  # Shared PDF upload
+                audio_file_name = gr.Textbox(label="Audio File Name (Leave blank to use PDF name)", value="", placeholder="Enter a custom name for the audio file")
+
+            with gr.Column() as col4:
+                preview_button = gr.Button(value="Refresh Preview")
+                generate_audio_button = gr.Button(value="Generate Audio")
+                combine_audio_button = gr.Button(value="Combine Audio Files")
+                stop_button = gr.Button(value="Stop")
+
+        with gr.Row() as row2:
+            preview_image = gr.Image(label="Content Area Preview")
+            with gr.Column() as col5:
+                header_slider = gr.Slider(label="Header Height", minimum=0, maximum=1000, value=50, step=1)
+                footer_slider = gr.Slider(label="Footer Height", minimum=0, maximum=1000, value=50, step=1)
+                page_range = gr.Textbox(label="Page Range (e.g., 1-3,5)", value="1", placeholder="Enter pages to process")
+                page_selector = gr.Number(label="Page Number", value=1, precision=0)  # Page selector for preview
+                output_dir = gr.Textbox(label="Output Directory", value="./demo_outputs/generated_audios")
 
 
-        with gr.Column() as col3:
-            speaker_name_studio
-            speaker_name_custom
-            output_dir = gr.Textbox(label="Output Directory", value="./demo_outputs/generated_audios")
-            generate_audio_button = gr.Button(value="Generate Audio")
-            stop_button = gr.Button(value="Stop")
-            stop_message = gr.Label(value="", label="Process Status")
+    combine_audio_button.click(  # Action for combining audio
+        fn=combine_audio_files,
+        inputs=[output_dir, gr.Textbox(value="combined_audio.wav", visible=False)],  # Output filename
+        outputs=[process_status],
+    )
 
+    # Preview Button Action
     preview_button.click(
         fn=preview_pdf_with_area,
         inputs=[pdf_file, header_slider, footer_slider, page_selector],
-        outputs=preview_image,
+        outputs=preview_image
     )
 
-    # Dynamically update preview based on sliders or page changes
+    # Generate Audio Button Action
+    generate_audio_button.click(
+        fn=generate_audio_from_pdf,
+        inputs=[pdf_file, output_dir, speaker_type, speaker_name_studio, cloned_speaker_names, lang, page_range,
+                header_slider, footer_slider, audio_file_name],
+        outputs=[process_status],
+    )
+
     header_slider.change(
         fn=preview_pdf_with_area,
         inputs=[pdf_file, header_slider, footer_slider, page_selector],
@@ -329,13 +365,6 @@ with gr.Blocks() as demo:
         inputs=[pdf_file, header_slider, footer_slider, page_selector],
         outputs=preview_image,
     )
-    # Preview button for manual refresh (optional)
-
-    preview_button.click(
-        fn=preview_pdf_with_area,
-        inputs=[pdf_file, header_slider, footer_slider, page_selector],
-        outputs=preview_image,
-    )
 
     # Preview Button Action
     page_selector.change(
@@ -344,35 +373,24 @@ with gr.Blocks() as demo:
         outputs=preview_image,
     )
 
-    # Preview Button Action
-    preview_button.click(
-        fn=preview_pdf_with_area,
-        inputs=[pdf_file, header_slider, footer_slider, page_selector],
-        outputs=preview_image
-    )
-    # Generate Audio Button Action
-    generate_audio_button.click(
-        fn=generate_audio_from_pdf,
-        inputs=[pdf_file, output_dir, speaker_type, speaker_name_studio, cloned_speaker_names, lang, page_range, header_slider, footer_slider],
-        outputs=[],
-    )
-
     clone_button.click(
         fn=clone_speaker,
         inputs=[upload_file, clone_speaker_name, cloned_speaker_names],
-        outputs=[upload_file, clone_speaker_name, cloned_speaker_names, speaker_name_custom],
+        outputs=[upload_file, clone_speaker_name, cloned_speaker_names, speaker_name_custom, process_status],
     )
 
     tts_button.click(
         fn=tts,
         inputs=[text, speaker_type, speaker_name_studio, speaker_name_custom, lang],
-        outputs=[generated_audio],
+        outputs=[generated_audio, process_status],
     )
+
+
 
     stop_button.click(
         fn=stop_process,
         inputs=[],
-        outputs=stop_message,
+        outputs=[process_status],
     )
 
 if __name__ == "__main__":
